c1f5cbdac1f3012ce0480384d5fe76ce4ec3adc9
Zwylair <82669735+Zwylair@users.noreply.github.com>
2024-10-11 12:49:48 +0300

    fix spawning minecraft files in program's dir

--- composeApp/scripts/run_minecraft.py
+++ composeApp/scripts/run_minecraft.py
@@ -5,11 +5,15 @@ import minecraft_launcher_lib
 minecraft_directory = "minecraft/"
 selected_version = ""
 
-for version in minecraft_launcher_lib.utils.get_installed_versions(minecraft_directory):
+cwd = os.getcwd()
+os.chdir(minecraft_directory)
+
+for version in minecraft_launcher_lib.utils.get_installed_versions(""):
     if "fabric" in version["id"] and "1.20.1" in version["id"]:
         selected_version = version["id"]
 
 options = minecraft_launcher_lib.utils.generate_test_options()
-minecraft_command = minecraft_launcher_lib.command.get_minecraft_command(selected_version, minecraft_directory, options)
+minecraft_command = minecraft_launcher_lib.command.get_minecraft_command(selected_version, "", options)
 
 subprocess.run(minecraft_command)
+os.chdir(cwd)
