68ecdc1a9f10630205baa80e1c4a56467d658057
Zwylair <82669735+Zwylair@users.noreply.github.com>
2024-10-11 16:20:34 +0300

    fix crashing when it is outdated settings json

--- composeApp/src/desktopMain/kotlin/org/zwylair/pisskaland_launcher/storage/LauncherSettings.kt
+++ composeApp/src/desktopMain/kotlin/org/zwylair/pisskaland_launcher/storage/LauncherSettings.kt
@@ -3,6 +3,8 @@ package org.zwylair.pisskaland_launcher.storage
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.MissingFieldException
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
@@ -11,32 +13,41 @@ import java.io.File
 val CONFIG_FILE = File("config.json")
 val launcherSettings = LauncherSettings()
 
-
 @Serializable
 private data class Settings(
-    val version: String = "1.0.0",
+    val version: String = "1.0.1",
+    var buildVersion: String,
     var nickName: String
 )
 
 class LauncherSettings() {
     var version by mutableStateOf("")
+    var buildVersion by mutableStateOf("")
     var nickName by mutableStateOf("")
 
     init { readConfig() }
 
-    fun saveConfig() {
-        CONFIG_FILE.writeText(Json.encodeToString(
-            Settings(
-                nickName = nickName
-            )
-        ))
+    private fun initEmpty(): Settings {
+        return Settings(
+            nickName = nickName,
+            buildVersion = buildVersion
+        )
     }
 
+    fun saveConfig() { CONFIG_FILE.writeText(Json.encodeToString(initEmpty())) }
+
+    @OptIn(ExperimentalSerializationApi::class)
     fun readConfig() {
         if (!CONFIG_FILE.exists()) { saveConfig() }
-        val settings = Json.decodeFromString<Settings>(CONFIG_FILE.readText())
+
+        var settings: Settings = initEmpty()
+
+        try {
+            settings = Json.decodeFromString<Settings>(CONFIG_FILE.readText())
+        } catch (e: MissingFieldException) { }
 
         nickName = settings.nickName
         version = settings.version
+        buildVersion = settings.buildVersion
     }
 }
